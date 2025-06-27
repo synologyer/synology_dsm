55f9919a0cc3d5c0109deb13d918a0d04e34fdb5
wanghuachen <150970520@huamei-tokyo.com>
2025-06-27 14:57:32 +0900

    delete

--- types/certificate.go
+++
@@ -1,9 +0,0 @@
-package types
-
-// 获取证书列表 响应参数
-// https://pve.proxmox.com/pve-docs/api-viewer/#/nodes/{node}/certificates/info
-type CertificateListResponse struct {
-	Data []struct {
-		Fingerprint string `json:"fingerprint"` // 当前证书的指纹
-	} `json:"data"`
-}
